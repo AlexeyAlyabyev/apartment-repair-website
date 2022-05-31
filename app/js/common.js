7cefe6333470adf920ae587b142c04a832b283a8
Rum Lux <7820625-rumlux@users.noreply.gitlab.com>
2022-05-31 14:41:12 +0300

    Калькулятор на мобилках

--- app/js/common.js
+++ app/js/common.js
@@ -82,8 +82,6 @@ $(function(){
 	}
 
 	// Слайдер типов потолков на главной
-	var any_types_of_ceiling_thumb = [];
-	var any_types_of_ceiling = [];
 	if ($(".any_types_of_ceiling").length) {
 		if (window.innerWidth < 992) {
 			const any_types_of_ceiling = new Swiper('.any_types_of_ceiling .wrapper .swiper.mobile', {
@@ -92,7 +90,10 @@ $(function(){
 				spaceBetween: 10,
 				slidesPerView: "auto",
 			});
+
 		} else {
+			var any_types_of_ceiling_thumb = [];
+			var any_types_of_ceiling = [];
 			$(".desktop .swiper_thumb").each(function(index){
 				any_types_of_ceiling_thumb[index] = new Swiper($(this)[0], {
 					loop: true,
@@ -120,13 +121,117 @@ $(function(){
 				any_types_of_ceiling[index].controller.control = any_types_of_ceiling_thumb[index];
 				any_types_of_ceiling_thumb[index].controller.control = any_types_of_ceiling[index];
 			});
+
+			// Переключение типов потолков на слайдере на главной
+			$(".any_types_of_ceiling .desktop .types_tumbler p").click(function(){
+				$(this).siblings("p").removeClass("active");
+				$(this).addClass("active");
+				$(".any_types_of_ceiling .desktop .item.active").removeClass("active");
+				$(".any_types_of_ceiling .desktop .item").eq($(this).index()).addClass("active");
+			});
 		}
 	}
-	
-	$(".any_types_of_ceiling .desktop .types_tumbler p").click(function(){
-		$(this).siblings("p").removeClass("active");
-		$(this).addClass("active");
-		$(".any_types_of_ceiling .desktop .item.active").removeClass("active");
-		$(".any_types_of_ceiling .desktop .item").eq($(this).index()).addClass("active");
+
+	// Мобильный калькулятор
+	var range_druggable = false;
+	var circle = $(".calc form.mobile .area_by_slider .filling .circle");
+	var filling = $(".calc form.mobile .area_by_slider .filling");
+	var filling_width = parseInt(filling.css("width"));
+	var current_value = $(".calc form.mobile .area_by_slider .filling .current_value");
+	var green_filling = $(".calc form.mobile .area_by_slider .filling .green");
+	var circle_pos = 0;
+	var left_percentage = 0;
+	var square_value = 20;
+	var final_price = 0;
+
+	recalculateWithoutMoving();
+
+	circle.on("mousedown",function(){
+		range_druggable = true;
 	});
+	$(document).on("mouseup",function(){
+		range_druggable = false;
+	});
+	$(document).on("mousemove",function(e){
+		if (range_druggable){
+			circle_pos = e.pageX - filling.prop("offsetLeft");
+			if (circle_pos < 0) circle_pos = 0;
+			if (circle_pos > filling_width) circle_pos = filling_width;
+			left_percentage = Math.floor(circle_pos / filling_width * 100);
+			square_value = Math.round(left_percentage / 100 * 90);
+			left_percentage += "%";
+			final_price = parseInt($(".calc form.mobile .ceiling_type p.active").attr("data-price")) * square_value;
+			circle.css("left", "calc(" + left_percentage + " - 25px/2)");
+			current_value.css("left", "calc(" + left_percentage + " - 18px/2)");
+			green_filling.css("width", left_percentage);
+			current_value.html(square_value);
+			$(".calc form.mobile .area_by_number input").val(square_value);
+			$(".calc form.mobile .price .value").html(final_price);
+			$(".calc input[name='summa']").val(final_price);
+		}
+	});
+	$(document).on("touchmove",function(e){
+		if (e.target == circle[0]){
+			circle_pos = e.originalEvent.changedTouches[0].clientX - filling.prop("offsetLeft");
+			if (circle_pos < 0) circle_pos = 0;
+			if (circle_pos > filling_width) circle_pos = filling_width;
+			left_percentage = Math.floor(circle_pos / filling_width * 100);
+			square_value = Math.round(left_percentage / 100 * 90);
+			left_percentage += "%";
+			final_price = parseInt($(".calc form.mobile .ceiling_type p.active").attr("data-price")) * square_value;
+			circle.css("left", "calc(" + left_percentage + " - 25px/2)");
+			current_value.css("left", "calc(" + left_percentage + " - 18px/2)");
+			green_filling.css("width", left_percentage);
+			current_value.html(square_value);
+			$(".calc form.mobile .area_by_number input").val(square_value);
+			$(".calc form.mobile .price .value").html(final_price);
+			$(".calc input[name='summa']").val(final_price);
+		}
+	});
+
+	$(".calc form.mobile .ceiling_type p").click(function(){
+		$(this).siblings("p.active").removeClass("active");
+		$(this).addClass("active");
+		recalculateWithoutMoving();
+	});
+
+	$(".calc form.mobile .area_by_number input[name='square']").on("change", recalculateWithMoving);
+
+	$("form").keydown(function(e){
+		if (e.keyCode == 13) {
+			e.preventDefault();
+			recalculateWithMoving();
+		}
+	});
+
+	function recalculateWithoutMoving(){
+		square_value = parseInt($(".calc form.mobile .area_by_number input[name='square']").val());
+		final_price = parseInt($(".calc form.mobile .ceiling_type p.active").attr("data-price")) * square_value;
+		$(".calc form.mobile .price .value").html(final_price);
+		$(".calc input[name='summa']").val(final_price);
+	}
+
+	function recalculateWithMoving(){
+		let square_input = $(".calc form.mobile .area_by_number input[name='square']");
+		square_value = parseInt(square_input.val());
+
+		if (square_value > 90) {
+			square_value = 90;
+			square_input.val(square_value);
+		}
+		if (square_value < 0) {
+			square_value = 0;			
+			square_input.val(square_value);
+		}
+
+		left_percentage = Math.floor(square_value / 90 * 100);
+		left_percentage += "%";
+		circle.css("left", "calc(" + left_percentage + " - 25px/2)");
+		current_value.css("left", "calc(" + left_percentage + " - 18px/2)");
+		green_filling.css("width", left_percentage);
+
+		final_price = parseInt($(".calc form.mobile .ceiling_type p.active").attr("data-price")) * square_value;
+		$(".calc form.mobile .price .value").html(final_price);
+		$(".calc input[name='summa']").val(final_price);
+	}
 });
